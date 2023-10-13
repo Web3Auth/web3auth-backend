3ff46285984131f99e75b09f0a12698c2d732b11
chaitanyapotti <chaitanya.potti@gmail.com>
2023-10-13 15:11:22 +0800

    update demo app

--- demo/node-app/index.js
+++ demo/node-app/index.js
@@ -1,7 +1,7 @@
 const { Web3Auth } = require("@web3auth/node-sdk");
 const { EthereumPrivateKeyProvider } = require("@web3auth/ethereum-provider");
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 const web3auth = new Web3Auth({
   clientId: "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ", // Get your Client ID from Web3Auth Dashboard
@@ -12,29 +12,29 @@ const ethereumProvider = new EthereumPrivateKeyProvider({
   config: {
     chainConfig: {
       chainId: "0x1",
-      rpcTarget: "https://rpc.ankr.com/eth"
-    }
-  }
+      rpcTarget: "https://rpc.ankr.com/eth",
+    },
+  },
 });
 
 web3auth.init({ provider: ethereumProvider });
 
-var privateKey = fs.readFileSync('privateKey.pem');
+var privateKey = fs.readFileSync("privateKey.pem");
 
 var sub = Math.random().toString(36).substring(7);
 
 var token = jwt.sign(
   {
     sub: sub,
-    name: 'Mohammad Yashovardhan Mishra Jang',
-    email: 'devrel@web3auth.io',
-    aud: 'urn:api-web3auth-io',
-    iss: 'https://web3auth.io',
+    name: "Mohammad Yashovardhan Mishra Jang",
+    email: "devrel@web3auth.io",
+    aud: "urn:api-web3auth-io",
+    iss: "https://web3auth.io",
     iat: Math.floor(Date.now() / 1000),
     exp: Math.floor(Date.now() / 1000) + 60 * 60,
   },
   privateKey,
-  { algorithm: 'RS256', keyid: '2ma4enu1kdvw5bo9xsfpi3gcjzrt6q78yl0h' },
+  { algorithm: "RS256", keyid: "2ma4enu1kdvw5bo9xsfpi3gcjzrt6q78yl0h" }
 );
 
 const connect = async () => {
@@ -45,5 +45,6 @@ const connect = async () => {
   });
   const eth_private_key = await provider.request({ method: "eth_private_key" });
   console.log("ETH Private Key: ", eth_private_key);
+  process.exit(0);
 };
 connect();
